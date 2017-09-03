06e40f68d2be6d618c26fe8b8e9a65f1234c0f1a
Jeferson <jefersonrod@yahoo.com.br>
2017-09-02 21:07:26 -0300

    Programa para extrair PDF de site HTML

---
+++ ExtraiPDF/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ExtraiPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btBusca = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lcLines = new System.Windows.Forms.Label();
+            this.rtbResult = new System.Windows.Forms.RichTextBox();
+            this.btLocalizar = new System.Windows.Forms.Button();
+            this.tbFile2Extract = new System.Windows.Forms.TextBox();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.btClear = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lcOccurrences = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btBusca
+            // 
+            this.btBusca.Location = new System.Drawing.Point(876, 20);
+            this.btBusca.Name = "btBusca";
+            this.btBusca.Size = new System.Drawing.Size(75, 23);
+            this.btBusca.TabIndex = 0;
+            this.btBusca.Text = "GO";
+            this.btBusca.UseVisualStyleBackColor = true;
+            this.btBusca.Click += new System.EventHandler(this.btAction_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 474);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Lines:";
+            // 
+            // lcLines
+            // 
+            this.lcLines.AutoSize = true;
+            this.lcLines.Location = new System.Drawing.Point(53, 474);
+            this.lcLines.Name = "lcLines";
+            this.lcLines.Size = new System.Drawing.Size(10, 13);
+            this.lcLines.TabIndex = 3;
+            this.lcLines.Text = "-";
+            // 
+            // rtbResult
+            // 
+            this.rtbResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbResult.Location = new System.Drawing.Point(13, 60);
+            this.rtbResult.Name = "rtbResult";
+            this.rtbResult.Size = new System.Drawing.Size(938, 411);
+            this.rtbResult.TabIndex = 4;
+            this.rtbResult.Text = "";
+            // 
+            // btLocalizar
+            // 
+            this.btLocalizar.Location = new System.Drawing.Point(15, 14);
+            this.btLocalizar.Name = "btLocalizar";
+            this.btLocalizar.Size = new System.Drawing.Size(75, 34);
+            this.btLocalizar.TabIndex = 5;
+            this.btLocalizar.Text = "Choose HTML File";
+            this.btLocalizar.UseVisualStyleBackColor = true;
+            this.btLocalizar.Click += new System.EventHandler(this.btLocalizar_Click);
+            // 
+            // tbFile2Extract
+            // 
+            this.tbFile2Extract.Location = new System.Drawing.Point(111, 20);
+            this.tbFile2Extract.Name = "tbFile2Extract";
+            this.tbFile2Extract.Size = new System.Drawing.Size(747, 20);
+            this.tbFile2Extract.TabIndex = 6;
+            this.tbFile2Extract.Text = "C:\\Windows\\temp\\enem.html";
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "openFileDialog1";
+            // 
+            // btClear
+            // 
+            this.btClear.Location = new System.Drawing.Point(870, 476);
+            this.btClear.Name = "btClear";
+            this.btClear.Size = new System.Drawing.Size(79, 19);
+            this.btClear.TabIndex = 7;
+            this.btClear.Text = "Clear";
+            this.btClear.UseVisualStyleBackColor = true;
+            this.btClear.Click += new System.EventHandler(this.btClear_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 474);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Occurrences";
+            // 
+            // lcOccurrences
+            // 
+            this.lcOccurrences.AutoSize = true;
+            this.lcOccurrences.Location = new System.Drawing.Point(194, 476);
+            this.lcOccurrences.Name = "lcOccurrences";
+            this.lcOccurrences.Size = new System.Drawing.Size(10, 13);
+            this.lcOccurrences.TabIndex = 9;
+            this.lcOccurrences.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(963, 496);
+            this.Controls.Add(this.lcOccurrences);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btClear);
+            this.Controls.Add(this.tbFile2Extract);
+            this.Controls.Add(this.btLocalizar);
+            this.Controls.Add(this.rtbResult);
+            this.Controls.Add(this.lcLines);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btBusca);
+            this.Name = "Form1";
+            this.Text = "Extract PDF from HTML v2  (14/07/2017)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btBusca;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lcLines;
+        private System.Windows.Forms.RichTextBox rtbResult;
+        private System.Windows.Forms.Button btLocalizar;
+        private System.Windows.Forms.TextBox tbFile2Extract;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.Button btClear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lcOccurrences;
+    }
+}
+
---
+++ ExtraiPDF/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExtraiPDF
+{
+    public partial class Form1 : Form
+    {
+        #region Variables
+        String htmlFile;
+        String Extension = ".pdf";
+        String HTTP = "http://";
+        String HREF = "href=";
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region Buttons
+        private void btAction_Click(object sender, EventArgs e)
+        {
+            //SearchReference();
+            SearchinWorking();
+        }
+        private void btLocalizar_Click(object sender, EventArgs e)
+        {
+            OpenFile();
+        }
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            rtbResult.Clear();
+            lcLines.Text = "";
+            tbFile2Extract.Text = "";
+            lcOccurrences.Text = "";
+        }
+        #endregion
+
+        #region Function OpenFile()
+        public void OpenFile()
+        {
+            ofd.InitialDirectory = @"C:\windows\temp";
+            ofd.Title = "Choose HTML file";
+            ofd.DefaultExt = "html";
+            ofd.Filter = "HTML (*.html)|*.html" +
+                "|HTM (*.htm)|*.htm";
+            ofd.CheckFileExists = true;
+            ofd.CheckPathExists = true;
+            ofd.FileName = "enem.html";
+
+            DialogResult dr = this.ofd.ShowDialog();
+
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+
+                tbFile2Extract.Text = ofd.FileName;
+            }
+
+        }
+        #endregion
+        #region Function SearchReference()
+        public void SearchReference()
+        {
+            htmlFile = tbFile2Extract.Text;
+            String content;
+            String line;
+            String lineLink;
+            int countLine = 0;
+            int startLine = 0;
+            int finishLine = 0;
+            int sizeLine = 0;
+            int sizeString = 0;
+            int startSubstring = 0;
+           
+            StreamReader str = new StreamReader(htmlFile);//Le o arquivo e joga dentro de str
+            while ((content = str.ReadLine()) != null)//Le o str linha a linha e joga em content até encontrar null que é o final do arquivo
+            {
+                countLine++; //contando as linhas
+                line = content; //recebendo o conteudo do html linha a linha
+                while (line.Contains(HTTP) && line.Contains(Extension)) //enquanto ainda tiver http e pdf na string continua a buscar links
+                {
+                    if (line.Contains(HTTP) && line.Contains(Extension) && line.Contains(HREF)) //verificando se a linha contem o http
+                    {
+                        sizeLine = line.Length; //pega o tamanho da variavel line
+                        String cabecalho = (HREF + "\"" + HTTP); // cria variavel para achar a sequencia href + http
+                        startLine = line.IndexOf(cabecalho) + HREF.Length + 1; //marca start da linha + tamanho do href
+                        sizeString = (sizeLine) - (startLine + 4); //define o tamanho da string
+                        lineLink = line.Substring(startLine, sizeString); //usa linelink para armazenar somente a parte que sera buscada o pdf
+                        sizeLine = lineLink.Length; //guarda o tamanho do linelink
+                        finishLine = lineLink.IndexOf(Extension); //marca o final do trecho onde esta o .pdf
+                        finishLine = finishLine + Extension.Length; //marca novamente mas adicionando o tamanho da string .pdf de 4 posições
+                        lineLink = lineLink.Substring(0, finishLine); //agora tira até a parte do pdf, separando o link
+                        startSubstring = line.IndexOf(Extension); //aqui marca onde esta o .pdf para depois tirar esta parte do line para remover a parte ja extraida
+                        rtbResult.AppendText(countLine + " | " + lineLink); //adiciona no textbox o numero da linha e | mais a linha
+                        rtbResult.AppendText("\n"); //pula linha no textbox
+                    }
+                    line = line.Substring(startSubstring+Extension.Length); //aqui retira do line a parte ja procurada para procurar mais links dentro do line
+                }
+            }
+            
+            lcLines.Text = Convert.ToString(countLine); //mostra a quantidade de linhas no label
+        }
+        #endregion
+        #region Function SearchinArray()
+        public void SearchinWorking()
+        {
+            htmlFile = tbFile2Extract.Text;
+            if (!htmlFile.ToUpper().Contains("HTML") || !htmlFile.ToUpper().Contains("HTM"))
+            {
+                MessageBox.Show("Favor informar o caminho do arquivo HTML\nValor não pode ser em branco");
+            }
+            else
+            {
+                
+                String content;
+                String line;
+                String lineLink;
+                int countLine = 0;
+                int startLine = 0;
+                int finishLine = 0;
+                int sizeLine = 0;
+                int sizeString = 0;
+                int startSubstring = 0;
+                String tempContent = "";
+                int countOccurrence = 0;
+
+                StreamReader str = new StreamReader(htmlFile);//Le o arquivo e joga dentro de str
+                while ((content = str.ReadLine()) != null)//Le o str linha a linha e joga em content até encontrar null que é o final do arquivo
+                {
+                    countLine++; //contando as linhas
+                    line = content; //recebendo o conteudo do html linha a linha
+                    while (line.Contains(HTTP) && line.Contains(Extension)) //enquanto ainda tiver http e pdf na string continua a buscar links
+                    {
+
+                        if (line.Contains(HTTP) && line.Contains(Extension) && line.Contains(HREF)) //verificando se a linha contem o http
+                        {
+
+                            sizeLine = line.Length; //pega o tamanho da variavel line
+                            String cabecalho = (HREF + "\"" + HTTP); // cria variavel para achar a sequencia href + http
+                            startLine = line.IndexOf(cabecalho) + HREF.Length + 1; //marca start da linha + tamanho do href
+                            sizeString = (sizeLine) - (startLine + 4); //define o tamanho da string
+                            lineLink = line.Substring(startLine, sizeString); //usa linelink para armazenar somente a parte que sera buscada o pdf
+                            sizeLine = lineLink.Length; //guarda o tamanho do linelink
+                            finishLine = lineLink.IndexOf(Extension); //marca o final do trecho onde esta o .pdf
+                            finishLine = finishLine + Extension.Length; //marca novamente mas adicionando o tamanho da string .pdf de 4 posições
+                            lineLink = lineLink.Substring(0, finishLine); //agora tira até a parte do pdf, separando o link
+                            startSubstring = line.IndexOf(Extension); //aqui marca onde esta o .pdf para depois tirar esta parte do line para remover a parte ja extraida
+
+
+                        
+                            for (int i = 0; i < lineLink.Length; i++) //começa laço para ler a variavel lineLink toda
+                            {
+                                tempContent = tempContent + lineLink[i]; //vai adicionando caracter a caracter a variavel tempContent
+                                if (tempContent.Contains(HTTP) && tempContent.Contains("\">"))
+                                {
+                                    int posNaoPDF = lineLink.IndexOf("\">"); //procura a posição do final da url sem pdf
+                                    lineLink = lineLink.Substring(posNaoPDF + 2); //remove da variavel a parte de url invalida
+                                    tempContent = ""; //limpa a variavel temporaria para receber novo valor                                
+                                    break; //interrompe a execução para ler novos valores
+
+                                }
+
+                                if (tempContent.Contains(HTTP) && !tempContent.Contains("\">") && tempContent.Contains(Extension)) //verificando se a linha contem o http e final do link ">
+                                {
+                                    try
+                                    {
+                                        rtbResult.AppendText(countLine + " | " + tempContent); //adiciona no textbox o numero da linha e | mais a linha
+                                        rtbResult.AppendText("\n"); //pula linha no textbox
+                                        tempContent = "";   //limpa a variavel temporaria para receber novo valor
+                                        countOccurrence++;
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        MessageBox.Show(ex.ToString());
+                                    }
+
+                                }
+
+                            }
+                        }
+                        line = line.Substring(startSubstring + Extension.Length); //aqui retira do line a parte ja procurada para procurar mais links dentro do line
+                    }
+                }
+                lcLines.Text = Convert.ToString(countLine); //mostra a quantidade de linhas no label
+                lcOccurrences.Text = Convert.ToString(countOccurrence); //mostra a quantidade de ocorrencias no label
+            }
+            
+        }
+        #endregion
+
+    }
+}
+
